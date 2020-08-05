adee1bc881b25bf01bd8552d6c4e3c321447c68e
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-08-05 14:09:09 +0200

    added axios, first axios request

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,15 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module! Variable x is ${x}`);
+import axios from 'axios';
+
+async function getResults(query){
+    try{
+        const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
+        console.log(res);
+    } catch(error){
+        alert(error);
+    }
+}
+
+getResults('pasta');
+
+//http://forkify-api.herokuapp.com/
+//https://forkify-api.herokuapp.com/api/get?rId=${this.id}
