7df5513892fb3fedeb83e588dd403935c0716ceb
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-08-06 14:37:10 +0200

    finishing recipe model - formatting ingredients array

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
@@ -24,14 +25,17 @@ const controlSearch = async () => {
         renderLoader(elements.searchRes);
 
 
+        try{
+            //4) Search for recipes
+            await state.search.getResults();
 
-        //4) Search for recipes
-        await state.search.getResults();
-
-        //5) Render results on UI
-        clearLoader();
-        searchView.renderResults(state.search.result);
-       
+            //5) Render results on UI
+            clearLoader();
+            searchView.renderResults(state.search.result);
+        } catch(error){
+            alert('Something wrong with the search...');
+            clearLoader();
+        }
 
     }
 }
@@ -49,3 +53,32 @@ elements.searchResPages.addEventListener('click', e => {
         searchView.renderResults(state.search.result, goToPage);
     }
 });
+
+const controlRecipe = async () => {
+    const id = window.location.hash.replace('#', '');
+
+    if(id){
+        //Prepare UI for changes
+
+        //Create new recipe object
+        state.recipe = new Recipe(id);
+        
+        try{
+
+            //Get recipe data
+            await state.recipe.getRecipe();
+
+            //Calculate servings and time
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+
+            //Render recipe
+            console.log(state.recipe);
+
+        } catch(err){
+            alert('Error processing recipe!');
+        }
+    }
+};
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
